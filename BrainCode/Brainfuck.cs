2e9314a114ec7fd587e215cf4e0dcc687e323b35
Juliana Pena <limited.mage@gmail.com>
2011-08-10 22:42:09 -0500

    I *think* the brainfuck interpreter is ready

--- BrainCode/Brainfuck.cs
+++ BrainCode/Brainfuck.cs
@@ -16,8 +16,10 @@ namespace BrainCode {
         private static readonly int STACK_SIZE = 1000;
 
         private string code;
-        private char[] tape;
         private int pc; // program counter
+
+        private char[] memory;
+        private int mc; // memory counter
 
         private int[] stack;
         private int sc; // stack counter
@@ -30,14 +32,98 @@ namespace BrainCode {
 
         public Brainfuck(string sourceCode, outputCallback outcb, inputCallback incb) {
             code = sourceCode;
-            tape = new char[MEMORY_SIZE];
+            memory = new char[MEMORY_SIZE];
             stack = new int[STACK_SIZE];
 
             pc = 0;
+            mc = 0;
             sc = 0;
 
             handleOutput = outcb;
             handleInput = incb;
+        }
+
+        public bool SanityCheck() {
+            int counter = 0;
+            foreach (char c in code) {
+                if (c == '[') {
+                    counter += 1;
+                } else if (c == ']') {
+                    counter -= 1;
+                    if (counter < 0) {
+                        return false;
+                    }
+                }
+            }
+
+            return counter == 0;
+        }
+
+        public void Run() {
+            while (pc < code.Length) {
+                RunOneStatement(code[pc]);
+            }
+        }
+
+        public void RunOneStatement(char c) {
+            switch (c) {
+                case '-':
+                    memory[mc] -= (char)1;
+                    pc += 1;
+                    break;
+                case '+':
+                    memory[mc] -= (char)1;
+                    pc += 1;
+                    break;
+                case '<':
+                    mc -= 1;
+                    pc += 1;
+                    break;
+                case '>':
+                    mc += 1;
+                    break;
+                case '.':
+                    handleOutput(memory[mc]);
+                    pc += 1;
+                    break;
+                case ',':
+                    memory[mc] = handleInput();
+                    pc += 1;    
+                    break;
+                case '[':
+                    OpenLoop();
+                    break;
+                case ']':
+                    CloseLoop();
+                    break;
+                default:
+                    // invalid character, continue
+                    pc += 1;
+                    break;
+            }
+        }
+
+        private void OpenLoop() {
+            if (memory[mc] == 0) {
+                // find loop close
+                int closeCounter = pc;
+                while (closeCounter < code.Length) {
+                    if (code[closeCounter] == ']') {
+                        pc = closeCounter + 1;
+                    }
+                }
+            } else {
+                // push stack
+                stack[sc] = pc;
+                sc += 1;
+                pc += 1;
+            }
+        }
+
+        private void CloseLoop() {
+            // pop stack
+            sc -= 1;
+            pc = stack[sc];
         }
     }
 }
